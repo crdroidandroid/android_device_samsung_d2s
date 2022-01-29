61099ea9e7774a199dfad86a49ff85d3e900fbbf
Orel <Orely4242@gmail.com>
2022-01-29 20:52:43 +0200

    d2s: adapt to crDroid

--- lineage_d2s.mk
+++ lineage_d2s.mk
@@ -17,32 +17,29 @@
 ## Inherit from generic products, most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-## Product API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-## Inscreen Fingerprint HAL
-TARGET_HAVE_FOD := true
+## Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Inherit from d2s device
 $(call inherit-product, device/samsung/d2s/device.mk)
+
+## Inherit some common crDroid stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Boot Animation
 TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
-
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-## Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+TARGET_HAVE_FOD := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier, this must come after all inclusions
 PRODUCT_NAME := lineage_d2s
 PRODUCT_DEVICE := d2s
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-N975F
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy Note10+
+PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
